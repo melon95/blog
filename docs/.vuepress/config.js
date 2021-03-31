1486320a010ce259f929727298ecb965759a08ef
melon95 <lijunwork95@gmail.com>
2021-03-31 20:00:34 +0800

    config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -33,12 +33,14 @@ module.exports = {
       { text: 'Home', link: '/' },
       { text: 'HTML', link: '/html/' },
       { text: 'Javascript', link: '/javascript/数据类型检测与转换' },
+      { text: 'HTTP', link: '/http/HTTP' },
       { text: 'Node', link: '/node/' },
       {
-        text: 'Vue',
+        text: 'Vue系列',
         items: [
-          { text: 'Chinese', link: '/language/chinese/' },
-          { text: 'Japanese', link: '/language/japanese/' }
+          { text: 'Vue', link: '/vue/计算属性和侦听属性' },
+          { text: 'VueRouter', link: '/vuerouter/VueRouter类' },
+          { text: 'VueX', link: '/vuex/VueX' }
         ]
       }
     ],
@@ -120,6 +122,74 @@ module.exports = {
         {
           title: '跨域解决方案',
           path: '/javascript/跨域解决方案',
+        },
+      ],
+      '/http/': [
+        {
+          title: 'HTTP',
+          path: '/http/HTTP',
+        },
+        {
+          title: 'HTTP结构',
+          path: '/http/HTTP结构',
+        },
+        {
+          title: 'HTTP状态码',
+          path: '/http/HTTP状态码',
+        },
+        {
+          title: 'HTTP缓存',
+          path: '/http/HTTP缓存',
+        },
+        {
+          title: 'TCP三次握手与四次挥手',
+          path: '/http/TCP三次握手与四次挥手',
+        },
+      ],
+      '/vue/': [
+        {
+          title: '计算属性和侦听属性',
+          path: '/vue/计算属性和侦听属性',
+        },
+        {
+          title: '响应式原理',
+          path: '/vue/响应式原理',
+        },
+        {
+          title: 'nextTick',
+          path: '/vue/nextTick',
+        },
+        {
+          title: 'keep-alive',
+          path: '/vue/keep-alive',
+        },
+        {
+          title: 'patch',
+          path: '/vue/patch',
+        },
+      ],
+      '/vuerouter/': [
+        {
+          title: 'VueRouter类',
+          path: '/vuerouter/VueRouter类',
+        },
+        {
+          title: 'VueRouter注册',
+          path: '/vuerouter/VueRouter注册',
+        },
+        {
+          title: 'VueRouter跳转',
+          path: '/vuerouter/VueRouter跳转',
+        },
+        {
+          title: '组件更新',
+          path: '/vuerouter/组件更新',
+        },
+      ],
+      '/vuex/': [
+        {
+          title: 'VueX',
+          path: '/vuex/VueX',
         },
       ],
     }
